b11817084905208632053ed03ee36667ea3542d1
carward25 <83989145+carward25@users.noreply.github.com>
2021-12-02 14:38:03 -0800

    Update TheThreeGame.py

--- TheThreeGame.py
+++ TheThreeGame.py
@@ -12,33 +12,41 @@ class TheThreeGame():
     def __init__(self):
         self.__p1_moves = []
         self.__p2_moves = []
-        self.__state = 'UNFINISHED'
+        self.__game_state = 'GAME_UNFINISHED'
 
-    def make_move(self, player, pick):
-        if (pick < 1 or pick > 10):
-            print('Error: You can only pick numbers 1 to 9')
-            return False
-        elif self.__state == 'is_it_a_draw' or self.__state == 'FIRST_WON' or self.__state == 'SECOND_WON':
-            print('Error: Game is over.')
-            return False
-        if player == 'first':
-            if pick in self.__p1_moves or pick in self.__p2_moves:
-                print('Number:', pick, 'already used.')
-            else:
-                self.__p1_moves.append(pick)
-        elif player == 'second':
-            if pick in self.__p1_moves or pick in self.__p2_moves:
-                print('Number:', pick, 'already used.')
-            else:
-                self.__p2_moves.append(pick)
+    def make_move(self, player, num):
+        if self.__game_state != 'GAME_UNFINISHED': return False
+        if player == 'first_player':
+            if 1 <= num <= 9 and num not in self.__p1_moves and num not in self.__p2_moves:
+                self.__p1_moves.append(num)
+                return True
+        elif player == 'second_player':
+            if 1 <= num <= 9 and num not in self.__p1_moves and num not in self.__p2_moves:
+                self.__p2_moves.append(num)
+                return True
+        self.get_winner()
+        return False
 
-        if sum(self.__p1_moves) == 15:
-            self.__state = 'FIRST_WON'
-        elif sum(self.__p2_moves) == 15:
-            self.__state = 'SECOND_WON'
-        elif len(self.__p1_moves) + len(self.__p2_moves) == 9:
-            self.__state = 'is_it_a_draw'
-        return True
+    def get_winner(self):
 
-    def get_current_state(self):
-        return self.__state
+        for a in self.__p1_moves:
+            for b in self.__p1_moves:
+                for c in self.__p1_moves:
+                    if (a + b + c) == 15 and a != b and a != c and b != c:
+                        self.__game_state = 'FIRST_PLAYER_WON'
+                        return self.__game_state
+        for a in self.__p2_moves:
+            for b in self.__p2_moves:
+                for c in self.__p2_moves:
+                    if (a + b + c) == 15 and a != b and a != c and b != c:
+                        self.__game_state = 'SECOND_PLAYER_WON'
+                        return self.__game_state
+        if len(self.__p1_moves) + len(self.__p2_moves) == 9:
+            self.__game_state = 'IT_IS_A_DRAW'
+        else:
+            self.__game_state = 'GAME_UNFINISHED'
+        return self.__game_state
+
+    def is_it_a_draw(self):
+        winner = self.get_winner()
+        return winner == 'IT_IS_A_DRAW'
