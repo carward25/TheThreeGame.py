d02d0aa089461d1d66c5ceb48657ec0562f93afb
carward25 <83989145+carward25@users.noreply.github.com>
2021-12-02 22:20:13 -0800

    Update TheThreeGame.py

--- TheThreeGame.py
+++ TheThreeGame.py
@@ -6,43 +6,45 @@
 # to exactly 15, then that player has won. If all numbers from 1-9 are chosen, but neither
 # player has won, then the game ends in a draw
 
-
 class TheThreeGame():
 
     def __init__(self):
-        self.__p1_numbers = []
-        self.__p2_numbers = []
-        self.__current_state = 'GAME_UNFINISHED'
+        self.__p1_moves = []
+        self.__p2_moves = []
+        self.__game_state = 'GAME_UNFINISHED'
 
     def make_move(self, player, num):
-        if self.__current_state != 'GAME_UNFINISHED': return False
+        if self.__game_state != 'GAME_UNFINISHED': return False
         if player == 'first_player':
-            if 1 <= num <= 9 and num not in self.__p1_numbers and num not in self.__p2_numbers:
-                self.__p1_numbers.append(num)
+            if 1 <= num <= 9 and num not in self.__p1_moves and num not in self.__p2_moves:
+                self.__p1_moves.append(num)
+                return True
         elif player == 'second_player':
-            if 1 <= num <= 9 and num not in self.__p1_numbers and num not in self.__p2_numbers:
-                self.__p2_numbers.append(num)
+            if 1 <= num <= 9 and num not in self.__p1_moves and num not in self.__p2_moves:
+                self.__p2_moves.append(num)
+                return True
         self.get_winner()
+        return False
 
     def get_winner(self):
 
-        for a in self.__p1_numbers:
-            for b in self.__p1_numbers:
-                for c in self.__p1_numbers:
+        for a in self.__p1_moves:
+            for b in self.__p1_moves:
+                for c in self.__p1_moves:
                     if (a + b + c) == 15 and a != b and a != c and b != c:
-                        self.__current_state = 'FIRST_PLAYER_WON'
-                        return self.__current_state
-        for a in self.__p2_numbers:
-            for b in self.__p2_numbers:
-                for c in self.__p2_numbers:
+                        self.__game_state = 'FIRST_PLAYER_WON'
+                        return self.__game_state
+        for a in self.__p2_moves:
+            for b in self.__p2_moves:
+                for c in self.__p2_moves:
                     if (a + b + c) == 15 and a != b and a != c and b != c:
-                        self.__current_state = 'SECOND_PLAYER_WON'
-                        return self.__current_state
-        if len(self.__p1_numbers) + len(self.__p2_numbers) == 9:
-            self.__current_state = 'IT_IS_A_DRAW'
+                        self.__game_state = 'SECOND_PLAYER_WON'
+                        return self.__game_state
+        if len(self.__p1_moves) + len(self.__p2_moves) == 9:
+            self.__game_state = 'IT_IS_A_DRAW'
         else:
-            self.__current_state = 'GAME_UNFINISHED'
-        return self.__current_state
+            self.__game_state = 'GAME_UNFINISHED'
+        return self.__game_state
 
     def is_it_a_draw(self):
         winner = self.get_winner()
